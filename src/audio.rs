a73cc1d2cf383d1d15735489046414506593cadf
neunyan <soundrabbitstd@gmail.com>
2019-10-04 10:49:06 +0900

    append audionodes

--- src/audio.rs
+++ src/audio.rs
@@ -1,6 +1,9 @@
 pub mod audio_node;
 
-pub enum Connection {}
+pub enum Connection {
+    Nodes(Vec<Connection>),
+    Node(AudioNode),
+}
 
 pub struct AudioNode {
     pub node: AudioNodeType,
@@ -245,5 +248,55 @@ impl PannerNode {
             ref_distance: None,
             rolloff_factor: None,
         }
+    }
+
+    pub fn cone_inner_angle(mut self, value: f64) -> Self {
+        self.cone_inner_angle = Some(value);
+        self
+    }
+
+    pub fn cone_outer_angle(mut self, value: f64) -> Self {
+        self.cone_outer_angle = Some(value);
+        self
+    }
+
+    pub fn cone_outer_gain(mut self, value: f64) -> Self {
+        self.cone_outer_gain = Some(value);
+        self
+    }
+
+    pub fn distance_model(mut self, value: DistanceModelType) -> Self {
+        self.distance_model = Some(value);
+        self
+    }
+
+    pub fn max_distance(mut self, value: f64) -> Self {
+        self.max_distance = Some(value);
+        self
+    }
+
+    pub fn orientation(mut self, x: f64, y: f64, z: f64) -> Self {
+        self.orientation = Some((x, y, z));
+        self
+    }
+
+    pub fn panning_model(mut self, value: PanningModelType) -> Self {
+        self.panning_model = Some(value);
+        self
+    }
+
+    pub fn position(mut self, x: f64, y: f64, z: f64) -> Self {
+        self.position = Some((x, y, z));
+        self
+    }
+
+    pub fn ref_distance(mut self, x: f64, y: f64, z: f64) -> Self {
+        self.ref_distance = Some((x, y, z));
+        self
+    }
+
+    pub fn rolloff_factor(mut self, value: f64) -> Self {
+        self.rolloff_factor = Some(value);
+        self
     }
 }
