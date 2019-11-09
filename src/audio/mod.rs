c2e7672395575c3e30d434ba7c8966b3a32461d8
neunyan <soundrabbitstd@gmail.com>
2019-11-09 16:28:28 +0900

    Update mod.rs

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -5,21 +5,3 @@ pub enum Node {
     Oscillator(node::Oscillator),
     Destination(node::Destination),
 }
-
-pub enum Spliter {
-    None,
-    Only(u64),
-    Equally,
-}
-
-pub enum Merger {
-    None,
-    Only(u64),
-    Equally,
-}
-
-pub enum Connection {
-    Pipeline(Vec<Connection>),
-    Branch(Spliter, Vec<Connection>, Merger),
-    Node(Node),
-}
