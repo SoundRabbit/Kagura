be7fafdec78073b27cb0bde04e45ae387da08580
neunyan <soundrabbitstd@gmail.com>
2019-11-22 12:58:38 +0900

    Delete panicable code from event handler

--- src/dom/component.rs
+++ src/dom/component.rs
@@ -163,8 +163,6 @@ where
                     dom_events.add(name, move |e| {
                         if let Some(me) = me.upgrade() {
                             me.borrow_mut().update(Box::new(handler(e)));
-                        } else {
-                            panic!("");
                         }
                     });
                 }
