5cef0cdec08b29bd1f1c217000f398c85aa02852
neunyan <soundrabbitstd@gmail.com>
2019-11-22 09:22:29 +0900

    Add debug message

--- src/task.rs
+++ src/task.rs
@@ -1,8 +1,10 @@
 use std::cell::RefCell;
+use wasm_bindgen::JsValue;
 
 thread_local!(static TASKS: RefCell<Vec<Box<dyn FnOnce()>>> = RefCell::new(vec![]));
 
 pub fn dispatch() {
+    web_sys::console::log_1(&JsValue::from("task::dispatch"));
     let tasks: Vec<_> = TASKS.with(|tasks| tasks.borrow_mut().drain(..).collect());
     for task in tasks {
         task();
@@ -10,6 +12,7 @@ pub fn dispatch() {
 }
 
 pub fn add(task: impl FnOnce() + 'static) {
+    web_sys::console::log_1(&JsValue::from("task::add"));
     TASKS.with(|tasks| {
         tasks.borrow_mut().push(Box::new(task));
     });
