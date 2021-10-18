9d4d3ae9fe2d668ed0d9058817cbb97a1a66c853
SoundRabbit <soundrabbitstd@gmail.com>
2021-10-18 13:29:31 +0900

    feat: update prelude

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,12 @@ pub mod kagura;
 mod libs;
 mod state;
 
+pub use html::component;
 pub use html::Html;
 pub use kagura::Kagura;
 
 pub mod prelude {
-    pub use crate::html::component::{self, Constructor, PrepackedComponent, Render, Update};
+    pub use crate::html::component::{Constructor, PrepackedComponent, Render, Update};
     pub use crate::html::{self, Attributes, Component, Events};
     pub use crate::Html;
     pub use crate::Kagura;
