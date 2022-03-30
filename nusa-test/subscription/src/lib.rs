bfc0297a08f2a55d29c01c8f8beab4e66a84e13d
SoundRabbit <soundrabbitstd@gmail.com>
2022-03-30 17:24:40 +0900

    test: subscript component

---
+++ nusa-test/subscription/src/lib.rs
@@ -0,0 +1,33 @@
+extern crate js_sys;
+extern crate kagura;
+extern crate nusa;
+extern crate wasm_bindgen;
+extern crate wasm_bindgen_futures;
+extern crate web_sys;
+
+use nusa::html_component::Sub;
+use nusa::HtmlComponent;
+use wasm_bindgen::prelude::*;
+
+mod child;
+mod parent;
+
+use parent::Parent;
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    wasm_bindgen_futures::spawn_local(kagura::Runtime::run(nusa::dom_node::BasicDomNode::new(
+        entry_point(),
+        |this| vec![Parent::empty(this, None, parent::Props {}, Sub::none())],
+    )));
+}
+
+fn entry_point() -> web_sys::Node {
+    web_sys::window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .get_element_by_id("app")
+        .unwrap()
+        .into()
+}
